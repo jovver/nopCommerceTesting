2906e8876d1d918b4ae4f3a6157b6d8b27e14640
Jon Andre V. Vergara <joreveroj@gmail.com>
2026-03-19 00:24:38 +0800

    Set-up

---
+++ pages/homepage.ts
@@ -0,0 +1,24 @@
+import { type Locator, type Page, expect } from '@playwright/test';
+
+export class Homepage {
+    readonly page: Page;
+    readonly getRegisterButton: Locator;
+    readonly getHomePageTitle: RegExp;
+
+    constructor(page: Page) {
+        this.page = page;
+        this.getRegisterButton = page.getByRole('link', { name: 'Register' });
+        this.getHomePageTitle = /nopCommerce demo store. Home page title/;
+    }
+
+    async clickRegisterButton() {
+        await this.getRegisterButton.click();
+    }
+
+    async assertPageTitle() {
+        await expect(this.page).toHaveTitle(this.getHomePageTitle);
+    }
+
+}
+
+export default Homepage;
