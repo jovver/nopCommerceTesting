b88b29859e8de0646e587ca4bd2d225548fec119
Jon Andre V. Vergara <joreveroj@gmail.com>
2026-03-22 12:02:36 +0800

    Quick updates

--- playwright.config.ts
+++ playwright.config.ts
@@ -39,7 +39,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    baseURL: String(process.env.BASE_URL),
+    baseURL: process.env.ENV === 'Base'? String(process.env.BASE_URL) : baseEnvURL.local.home,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
